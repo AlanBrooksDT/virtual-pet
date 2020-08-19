e242512f29e07970f8a353b3856500d3d1c64afa
Alan Brooks <abrooks@live.co.uk>
2020-08-19 21:50:11 +0100

    vaguely understanding what I just did

--- tests/pet.test.js
+++ tests/pet.test.js
@@ -1,7 +1,14 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    it('returns an object', () => {
+    test('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
   });
+
+describe('new pet', () => {
+    test('creates new pet instance with name', () => {
+        const pet = new Pet('Fido');
+        expect(pet.name).toEqual('Fido');
+    })
+});
