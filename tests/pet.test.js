86ac68408d3aed4a4c8b264fe361050ac33f2912
Alan Brooks <abrooks@live.co.uk>
2020-08-19 22:42:20 +0100

    test scripts and added prototype methods

--- tests/pet.test.js
+++ tests/pet.test.js
@@ -12,3 +12,39 @@ describe('new pet', () => {
         expect(pet.name).toEqual('Fido');
     })
 });
+
+describe('initial age 0', () => {
+    test('initial age is 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toEqual(0);
+    })
+});
+
+describe('increase age', () => {
+    it('increase the age by 1', () => {
+      const pet = new Pet('Fido');
+  
+      pet.growUp();
+  
+      expect(pet.age).toEqual(1);
+    });
+  });
+  describe('initial hunger and fitnes', () => {
+    test('initial hunger is 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toEqual(0);
+    });
+    test('fitness = 10', () => {
+        const pet = new Pet('Fido');
+        expect(pet.fitness).toEqual(10);
+    });
+});
+
+describe ('getting older strains', () => {
+    test('losing fitness with age', () => {
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
+    });
+});
